34883487977304e63773b1f4c84f388d238851b4
Anna <k24i3@mail.ru>
2022-07-08 10:56:14 +0400

    add Example_6_Base

---
+++ Example_6_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 4;
+int b = 20;
+int c = 1;
+int d = 5;
+int e = 6;
+
+int max = a;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+if ( d > max ) max = d;
+if ( e > max ) max = e;
+
+Console.Write("max=");
+Console.WriteLine(max);
